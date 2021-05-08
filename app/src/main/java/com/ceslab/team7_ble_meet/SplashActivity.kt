514f01bb6a23660aaba2c935e74291bfbef955d3
phattan204 <phattan2041999@gmail.com>
2021-05-08 22:23:34 +0700

    config ripple + splash

--- app/src/main/java/com/ceslab/team7_ble_meet/SplashActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.ceslab.team7_ble_meet
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.ceslab.team7_ble_meet.dashboard.DashBoardActivity
 
@@ -13,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         var intent = Intent(this@SplashActivity, DashBoardActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
         Handler().postDelayed({
             startActivity(intent)
