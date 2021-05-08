edd408651d2625c1c77cfbc79a92ace5cf952083
DESKTOP-CKOM2EH\Killua Nguyen <Khanh>
2021-05-08 15:24:54 +0700

    no message

--- app/src/main/java/com/ceslab/team7_ble_meet/profile/ProfileActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/profile/ProfileActivity.kt
@@ -1,42 +1,48 @@
 package Activity
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.Spinner
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.ceslab.team7_ble_meet.R
+import kotlinx.android.synthetic.main.custom_spinner_gender.*
+import kotlinx.android.synthetic.main.custom_spinner_gender.view.*
 
 
 class ProfileActivity : AppCompatActivity() {
-    lateinit var spi_genner : Spinner
+    private lateinit var spinner_gender : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        spi_genner = findViewById(R.id.spi_gender) as Spinner
-        val option_gender = resources.getStringArray(R.array.gender_spinner);
-        spi_genner.adapter = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_1,
-            option_gender
-        )
-        spi_genner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
+         spinner_gender = findViewById(R.id.spinner_genner)
+        spinner_gender.setOnClickListener {
+            //inflate the dialog with cutomview
+            val  Dialog_View = LayoutInflater.from(this).inflate(R.layout.custom_spinner_gender,null)
+            //Builder AlertDialog
+            val mBuilder = AlertDialog.Builder(this).setView(Dialog_View)
+            //show Dialog
+            val mAlertDialog = mBuilder.show()
+            Dialog_View.text_Men.setOnClickListener {
+                    val men = Dialog_View.text_Men.text.toString()
+                spinner_gender.setText(men)
+                mAlertDialog.dismiss()
+            }
+            Dialog_View.text_Girl.setOnClickListener {
+                val girl = Dialog_View.text_Girl.text.toString()
+                spinner_gender.setText(girl)
+                mAlertDialog.dismiss()
             }
 
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-
-            }
         }
 
+
+    }
+    private fun setDialog(){
+            android.R.layout.simple_list_item_multiple_choice
     }
 }
