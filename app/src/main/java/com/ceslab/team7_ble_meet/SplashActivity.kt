7830b61edfdc6f610268841d22180d3913f4f67f
phattan204 <phattan2041999@gmail.com>
2021-05-08 16:47:20 +0700

    improve splash

--- app/src/main/java/com/ceslab/team7_ble_meet/SplashActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil.setContentView
 import com.ceslab.team7_ble_meet.dashboard.DashBoardActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -12,11 +11,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        var intent = Intent(this@SplashActivity,DashBoardActivity::class.java).apply {
+        var intent = Intent(this@SplashActivity, DashBoardActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         Handler().postDelayed({
             startActivity(intent)
-        },500)
+        }, 2200)
     }
+    override fun onPause() {
+        super.onPause()
+        overridePendingTransition(0, 0)
+    }
+
 }
