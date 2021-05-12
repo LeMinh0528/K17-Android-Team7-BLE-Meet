e3324ea41db8fae92781f1d2e1cb644c2a1f96fa
DESKTOP-CKOM2EH\Killua Nguyen <Khanh>
2021-05-12 22:04:51 +0700

    up screen  fragment profile , actuvity profile

--- app/src/main/java/com/ceslab/team7_ble_meet/profile/EditProfileActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/profile/EditProfileActivity.kt
@@ -1,31 +1,20 @@
 package Activity
 
-import android.app.ActionBar
-import android.app.ProgressDialog.show
-import android.content.Context
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
 import com.ceslab.team7_ble_meet.EditDialog.Edit_Dialog
 import com.ceslab.team7_ble_meet.R
-import kotlinx.android.synthetic.main.custom_spinner_gender.*
-import kotlinx.android.synthetic.main.custom_spinner_gender.view.*
 
 
 class EditProfileActivity : AppCompatActivity() {
     private lateinit var spinner_gender : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_editprofile)
         spinner_gender = findViewById(R.id.spinner_genner)
         spinner_gender.setOnClickListener {
             //inflate the dialog with cutomview
