62bb5a96dc5d8e4a1b8daa1e7d00f0e88a6b7954
DESKTOP-CKOM2EH\Killua Nguyen <Khanh>
2021-05-13 22:23:54 +0700

    up register information gender and birthday , rename selector

---
+++ app/src/main/java/com/ceslab/team7_ble_meet/RegisterImformation/RegisterGenderActivity.kt
@@ -0,0 +1,12 @@
+package com.ceslab.team7_ble_meet.RegisterImformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ceslab.team7_ble_meet.R
+
+class RegisterGenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_gender)
+    }
+}
