f9ace7430131d5ea53658e6f374c9500d2c1acca
DESKTOP-CKOM2EH\Killua Nguyen <Khanh>
2021-05-15 20:52:03 +0700

    no message

---
+++ app/src/main/java/com/ceslab/team7_ble_meet/RegisterImformation/RegisterNickNameActivity.kt
@@ -0,0 +1,11 @@
+package com.ceslab.team7_ble_meet.RegisterImformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterNickNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
