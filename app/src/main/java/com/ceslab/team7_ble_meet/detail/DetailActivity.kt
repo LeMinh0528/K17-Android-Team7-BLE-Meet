d1a12ed908ea206eaa0682e7399c1e2558135db7
minhlenhat28 <lenhatminh0528@gmail.com>
2021-06-15 20:10:56 +0700

    fix information UI

--- app/src/main/java/com/ceslab/team7_ble_meet/detail/DetailActivity.kt
+++ app/src/main/java/com/ceslab/team7_ble_meet/detail/DetailActivity.kt
@@ -23,7 +23,7 @@ class DetailActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_detail)
-        binding.btnBackpress.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             finish()
         }
     }
